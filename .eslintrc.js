ac78381472d22901a04e8eeb960c79a9627508ba
Marko Seppänen <marko.p.seppanen@gmail.com>
2021-01-20 10:08:48 +0200

    eslint+prettier configured

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native'],
+  rules: {
+    'no-console': 0,
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react-native/no-inline-styles': 2,
+    'require-jsdoc': 0,
+  },
+};
