81c7bc281320e3db4a6776b25ada3cb98b693f21
Rikotee <teerek81@gmail.com>
2021-02-01 16:00:01 +0200

    Upload

--- views/Upload.js
+++ views/Upload.js
@@ -3,13 +3,30 @@ import {Platform, ScrollView} from 'react-native';
 import {Input, Text, Image, Button, Card} from 'react-native-elements';
 import useUploadForm from '../hooks/UploadHooks';
 import * as ImagePicker from 'expo-image-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useMedia} from '../hooks/ApiHooks';
 
 const Upload = () => {
   const [image, setImage] = useState(null);
+  const {upload} = useMedia();
 
   const {handleInputChange, inputs} = useUploadForm();
 
-  const doUpload = async () => {};
+  const doUpload = async () => {
+    const formData = new FormData();
+    // add text to formData
+    formData.append('title', inputs.title);
+    formData.append('description', inputs.description);
+    // add image to formData
+    formData.append('file', {uri: image, name: 'filename', type: 'image/jpeg'});
+    try {
+      const userToken = await AsyncStorage.getItem('userToken');
+      const resp = await upload(formData, userToken);
+      console.log('upload response', resp);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -66,7 +83,7 @@ const Upload = () => {
       />
       <Button title="Choose from library" onPress={() => pickImage(true)} />
       <Button title="Use camera" onPress={() => pickImage(false)} />
-      <Button title="Upload file" />
+      <Button title="Upload file" onPress={doUpload} />
     </ScrollView>
   );
 };
