be0031e30352137c591f7e8112919f661faf19f5
Rikotee <teerek81@gmail.com>
2021-02-08 09:36:39 +0200

    player

--- views/Single.js
+++ views/Single.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, ActivityIndicator} from 'react-native';
 import PropTypes from 'prop-types';
 import {uploadsUrl} from '../utils/variables';
 import {Avatar, Card, ListItem, Text} from 'react-native-elements';
 import moment from 'moment';
+import {useTag} from '../hooks/ApiHooks';
+import {Video} from 'expo-av';
 
 const Single = ({route}) => {
   const {file} = route.params;
+  const [avatar, setAvatar] = useState('http://placekitten.com/100');
+  const {getFilesByTag} = useTag();
+
+  useEffect(() => {
+    const fetchAvatar = async () => {
+      try {
+        const avatarList = await getFilesByTag('avatar_' + file.user_id);
+        if (avatarList.length > 0) {
+          setAvatar(uploadsUrl + avatarList.pop().filename);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    fetchAvatar();
+  }, []);
+
   return (
     <Card>
       <Card.Title h4>{file.title}</Card.Title>
       <Card.Title>{moment(file.time_added).format('LLL')}</Card.Title>
       <Card.Divider />
-      <Card.Image
-        source={{uri: uploadsUrl + file.filename}}
-        style={styles.image}
-        PlaceholderContent={<ActivityIndicator />}
-      />
+      <>
+        {file.media_type === 'image' ? (
+          <Card.Image
+            source={{uri: uploadsUrl + file.filename}}
+            style={styles.image}
+            PlaceholderContent={<ActivityIndicator />}
+          />
+        ) : (
+          <Video
+            source={{uri: uploadsUrl + file.filename}}
+            style={styles.image}
+            useNativeControls={true}
+          />
+        )}
+      </>
       <Card.Divider />
       <Text style={styles.description}>{file.description}</Text>
       <ListItem>
-        <Avatar source={{uri: 'http://placekitten.com/180'}} />
+        <Avatar source={{uri: avatar}} />
         <Text>Ownername</Text>
       </ListItem>
     </Card>
