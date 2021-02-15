e0365dbf348af5ea69fd6c6243e6878ecc3f2aa8
Rikotee <teerek81@gmail.com>
2021-02-15 17:07:50 +0200

    model

--- utils/variables.js
+++ utils/variables.js
@@ -1,5 +1,5 @@
 const uploadsUrl = 'https://media-new.mw.metropolia.fi/wbma/uploads/';
 const baseUrl = 'https://media-new.mw.metropolia.fi/wbma/';
-const appIdentifier = 'ThisIsMyAppIdentifierNotYours';
+const appIdentifier = 'Cookbook';
 
 export {uploadsUrl, baseUrl, appIdentifier};
