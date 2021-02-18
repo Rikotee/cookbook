a313f334e4b80bf639a4d07be4a525818de7ca46
Rikotee <teerek81@gmail.com>
2021-02-18 13:50:20 +0200

    Image and nonworking login button to home

--- views/Home.js
+++ views/Home.js
@@ -2,10 +2,22 @@ import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import List from '../components/List';
 import PropTypes from 'prop-types';
+import {Button} from 'react-native-elements';
+import {Image} from 'react-native';
+// import Login from '../views/Login';
 
 const Home = ({navigation}) => {
+  // const goLogin = () => {
+
+  // };
+
   return (
     <SafeAreaView style={styles.StyleSheet}>
+      <Image
+        source={require('../assets/TestLogo.jpg')}
+        style={styles.image}
+      ></Image>
+      <Button title="Login" />
       <List navigation={navigation} myFilesOnly={false} />
       <StatusBar style="auto" />
     </SafeAreaView>
