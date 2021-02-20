f64c1ecf94dfba21f2c34fa51393d86aeb218b85
Rikotee <teerek81@gmail.com>
2021-02-20 16:51:24 +0200

    homepage cards not yet fully working

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,17 +1,48 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {uploadsUrl} from '../utils/variables';
-import {Avatar, ListItem as RNEListItem} from 'react-native-elements';
+import {
+  Avatar,
+  Card,
+  Text,
+  ListItem as RNEListItem,
+} from 'react-native-elements';
 import {Button} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useMedia} from '../hooks/ApiHooks';
 import {MainContext} from '../contexts/MainContext';
 import {Alert} from 'react-native';
+import {useTag, useUser} from '../hooks/ApiHooks';
 
 const ListItem = ({navigation, singleMedia, isMyFile}) => {
   // console.log(props);
   const {deleteFile} = useMedia();
   const {setUpdate, update, isLoggedIn} = useContext(MainContext);
+  const [avatar, setAvatar] = useState('http://placekitten.com/100');
+  const {getFilesByTag} = useTag();
+  const [owner, setOwner] = useState({username: 'somebody'});
+  const {getUser} = useUser();
+  // const {file} = route.params;
+
+  const fetchAvatar = async () => {
+    try {
+      const avatarList = await getFilesByTag('avatar_' + singleMedia.user_id);
+      if (avatarList.length > 0) {
+        setAvatar(uploadsUrl + avatarList.pop().filename);
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  const fetchOwner = async () => {
+    try {
+      const userToken = await AsyncStorage.getItem('userToken');
+      const userData = await getUser(singleMedia.user_id, userToken);
+      setOwner(userData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
   const doDelete = () => {
     Alert.alert(
@@ -37,6 +68,11 @@ const ListItem = ({navigation, singleMedia, isMyFile}) => {
     );
   };
 
+  useEffect(() => {
+    fetchAvatar();
+    fetchOwner();
+  });
+
   return (
     <RNEListItem
       bottomDivider
@@ -44,24 +80,29 @@ const ListItem = ({navigation, singleMedia, isMyFile}) => {
         navigation.navigate('Single', {file: singleMedia});
       }}
     >
-      <Avatar
-        size="large"
-        square
-        source={{uri: uploadsUrl + singleMedia.thumbnails.w160}}
-      ></Avatar>
-      <RNEListItem.Content>
-        <RNEListItem.Title h4>{singleMedia.title}</RNEListItem.Title>
-        <RNEListItem.Subtitle>{singleMedia.description}</RNEListItem.Subtitle>
-        {isMyFile && isLoggedIn && (
-          <>
-            <Button
-              title="Modify"
-              onPress={() => navigation.push('Modify', {file: singleMedia})}
-            ></Button>
-            <Button title="Delete" color="red" onPress={doDelete}></Button>
-          </>
-        )}
-      </RNEListItem.Content>
+      <Card>
+        <Avatar source={{uri: avatar}} />
+        <Text>{owner.username}</Text>
+        <Avatar
+          size="large"
+          square
+          source={{uri: uploadsUrl + singleMedia.thumbnails.w160}}
+        ></Avatar>
+        <RNEListItem.Content>
+          <RNEListItem.Title h4>{singleMedia.title}</RNEListItem.Title>
+          <RNEListItem.Subtitle>{singleMedia.description}</RNEListItem.Subtitle>
+          {isMyFile && isLoggedIn && (
+            <>
+              <Button
+                title="Modify"
+                onPress={() => navigation.push('Modify', {file: singleMedia})}
+              ></Button>
+              <Button title="Delete" color="red" onPress={doDelete}></Button>
+            </>
+          )}
+        </RNEListItem.Content>
+      </Card>
+
       <RNEListItem.Chevron />
     </RNEListItem>
   );
@@ -71,6 +112,7 @@ ListItem.propTypes = {
   singleMedia: PropTypes.object,
   navigation: PropTypes.object,
   isMyFile: PropTypes.bool,
+  // route: PropTypes.object,
 };
 
 export default ListItem;
