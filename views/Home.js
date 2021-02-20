f0106ce7a0eac84b2160eb5c42636e79ae35df4f
Rikotee <teerek81@gmail.com>
2021-02-20 13:09:47 +0200

    some styling changes

--- views/Home.js
+++ views/Home.js
@@ -2,22 +2,20 @@ import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import List from '../components/List';
 import PropTypes from 'prop-types';
-import {Button} from 'react-native-elements';
 import {View} from 'react-native';
 import {Text} from 'react-native';
-// import Login from '../views/Login';
 
 const Home = ({navigation}) => {
   return (
     <SafeAreaView style={styles.StyleSheet}>
       <View style={styles.loginArea}>
-        <Text style={styles.loginText}>Login/Register</Text>
+        <Text
+          style={styles.loginText}
+          onPress={() => navigation.navigate('Login')}
+        >
+          Login/Register
+        </Text>
       </View>
-      <Button
-        title="Login/Register"
-        color="#FFF"
-        onPress={() => navigation.navigate('Login')}
-      />
       <List navigation={navigation} myFilesOnly={false} />
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -31,6 +29,7 @@ const styles = StyleSheet.create({
   },
   loginText: {
     color: '#FFF',
+    textAlign: 'right',
   },
 });
 
